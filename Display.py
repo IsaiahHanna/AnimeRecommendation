3da0834d1d0d8f0a4c7de2ad902c2bdfe5b26331
Isaiah Hanna <isaiah.hanna19@gmail.com>
2024-09-18 22:05:26 -0700

    Changed the output of similar anime so that the user would not be recommended the anime they inputted. Also fixed comments on Display.py

--- Display.py
+++ Display.py
@@ -5,16 +5,19 @@ Purpose: Functions used for UI
 '''
 from SimilarityScores import SimilarityScores
 
+#Take in user input and find row corresponding to that show
+#Capture user preferences or input, such as preferred genres or other criteria for recommendations.
+#Process the user input and convert it into a format compatible with the similarity calculation.
 def UserInput(originalDf,features):
-    #Take in user input and find row corresponding to that show
-    #Capture user preferences or input, such as preferred genres or other criteria for recommendations.
-    #Process the user input and convert it into a format compatible with the similarity calculation.
-
     userInput = input("What was your most recently watched anime? (Please spell out the show's full name) ").lower()
     userAnime = originalDf.loc[originalDf['title'] == userInput]
     userAnime = features.loc[features['uid'] == userAnime['uid'].iloc[0]]
     return userAnime
 
+
+#Recommendation Generation
+#Based on the user input and the calculated similarities, generate a list of recommended anime titles.
+#Rank the recommended titles based on their similarity to the user input and possibly other factors like popularity or member counts.
 def PrintSimilarAnime(originalDf,features,userAnime):
     top5MostSimilar = SimilarityScores(features,userAnime,5)
     for i in top5MostSimilar:
--- SimilarityScores.py
+++ SimilarityScores.py
@@ -13,10 +13,11 @@ Will be using Cosine Similarity
 Calculate the similarity between each pair of items in the dataset using the chosen metric.
 """
 
+#Input: features dataframe, UserAnime series (1 row dataframe) and number of Similar anime to produce  
+#Output: A dataframe with 
 def SimilarityScores(features,target,numRows): 
     target = np.array(target)
     features = np.array(features)
-
     cosine_sim = cosine_similarity(features,target).reshape(1, -1)[0]
-    similarEntries = cosine_sim.argsort()[::-1][:numRows]
+    similarEntries = cosine_sim.argsort()[::-1][1:numRows+1]
     return similarEntries
