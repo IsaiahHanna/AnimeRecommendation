1d008aea913f6903c3dda61c7cb2061b5bb917a1
IsaiahHanna <isaiah.hanna19@gmail.com>
2024-12-17 20:05:47 -0800

    Revert "Modified file structure to better suit flask app structure format."
    This reverts commit 2e1b51fce3373470887bdba1392cca2752d57548.
    
    Revert commit to change file structure.

--- webpage.py
+++ webpage.py
@@ -9,7 +9,6 @@ from Main import Recommendation
 import requests
 from ImportData.api_Url import URL
 from ExceptionsList import DataImportException
-from waitress import serve
 
 #Create instance of Recommendation object 
 recommender = Recommendation()
@@ -47,6 +46,5 @@ def index():
 def getShows():
     return jsonify(titlesDict)
 
-
-serve(app,host = '0.0.0.0',port = 8080)
+app.run(host = "0.0.0.0", port = 8080,debug=True) #Tells flask to run on my machine and which port
 
